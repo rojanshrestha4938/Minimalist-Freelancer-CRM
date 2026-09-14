d2b52f03f2efb1e7451c883bebcc31d866d1ee0a
Rojan Shrestha <shrestharojanpro@gmail.com>
2026-09-14 08:21:41 +0545

    We changed DEBUG = True in config/settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,8 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+# DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
